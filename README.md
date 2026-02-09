e0c36dd58de33e389ca5a9e863ceff8bf9b58835
Daniel Romero <danieljromero305@gmail.com>
2026-02-09 17:57:22 -0500

    XLSXToJSON.py

---
+++ README.md
@@ -0,0 +1,3 @@
+Usage: python XLSXToJSON.py <XLSXFile.xlsx> [sheet_number]
+
+sheet_number is 0 (first sheet) by default
--- XLSXToJSON.py
+++ XLSXToJSON.py
@@ -1,5 +1,42 @@
 import openpyxl
 import sys
+import json
 
 if __name__ == '__main__':
-    print(sys.argv)
+    sheet_number = 0
+    if len(sys.argv) not in [2, 3]:
+        print("Usage: python XLSXToJSON.py <XLSXFile.xlsx> [sheet_number = 0]")
+        exit(1)
+
+    if len(sys.argv) == 3:
+        try:
+            sheet_number = int(sys.argv[2])
+        except ValueError:
+            print("Invalid sheet number")
+            exit(1)
+
+    wb = openpyxl.load_workbook(sys.argv[1])
+    ws = wb.worksheets[sheet_number]
+    print(ws)
+
+    max_col = 0
+    headers = []
+    json_list = []
+    for row_num, row in enumerate(ws.iter_rows()):
+        row_dict = {}
+        for cell in row:
+            if cell.value is not None:
+                if row_num == 0:
+                    headers.append(cell.value)
+                    if max_col < cell.column:
+                        max_col = cell.column
+                else:
+                    row_dict[headers[cell.column - 1]] = cell.value
+        if row_dict:
+            json_list.append(row_dict)
+
+    # print(json_list)
+    with open('output.json', 'w') as json_file:
+        json.dump(json_list, json_file, indent=4)
+
+    print("output.json created")
